54b2463d7427abf84dbf6db2d4ff540f299b0b7a
Lorenzo Sánchez Fraile <sanchezfrailelorenzo@gmail.com>
2026-07-05 15:52:12 +0200

    update changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Apero.MixProject do
   def project do
     [
       app: :apero,
-      version: "0.2.1",
+      version: "2.0.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
